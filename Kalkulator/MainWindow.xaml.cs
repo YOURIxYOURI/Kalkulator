b0ee1a8ff42d68a1bf939340bbc7e2c96e6f41fb
YOURIxYOURI <97059905+YOURIxYOURI@users.noreply.github.com>
2023-05-09 10:01:16 +0200

    na chuj mi to

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -28,15 +28,54 @@ namespace Kalkulator
         public MainWindow()
         {
             InitializeComponent();
-            check = false;
-            check2 = true;
             equal_click = false;
             task = "";
             equal = .0;
-            symbols.AddRange(new string[] { "+", "-", "=", "x", "/", "C", "<-" });
+            symbols.AddRange(new string[] { "+", "-", "=", "x", "/", "C", "<-", "n!","x^y","sqrt"});
+        }
+        public double Factorial(double n)
+        {
+            double i, fact = 1, number = n;
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //try
+        //{
+        //    var data = ((Button)sender).Content.ToString();
+        //    if (data == "+/-")
+        //    {
+        //        double d = Convert.ToDouble(screen.Text);
+        //        d *= -1;
+        //        screen.Text = d.ToString();
+        //    }
+        //    else if (data == "C")
+        //    {
+        //        task = "";
+        //        equal = .0;
+        //        screen.Text = "";
+        //    }
+        //    else if (data == "<-")
+        //    {
+        //        string s = screen.Text;
+        //        if (s.Length > 1)
+        //            s = s.Substring(0, s.Length - 1);
+        //        else
+        //            s = "";
+        //        screen.Text = s;
+        //    }
+        //    else
+        //    {
+        //        screen.Text += data;
+        //    }
+        //}
+        //catch (Exception exc) { MessageBox.Show("U are too dumb to even use a calculator and this is why" + exc.Message, "You dumb", MessageBoxButton.OK, MessageBoxImage.Warning); }
+        //}
         {
             try
             {
@@ -52,7 +91,7 @@ namespace Kalkulator
 
                 if (symbols.Contains(data))
                 {
-                    
+
                     if (task != "" && check2)
                     {
                         switch (task)
@@ -61,31 +100,34 @@ namespace Kalkulator
                             case "-": equal -= Convert.ToDouble(screen.Text); break;
                             case "x": equal *= Convert.ToDouble(screen.Text); break;
                             case "/": equal /= Convert.ToDouble(screen.Text); break;
+                            case "sqrt": equal = Math.Sqrt(Convert.ToDouble(screen.Text)); break;
+                            case "n!": equal = Factorial(Convert.ToDouble(screen.Text)); break;
+                            case "x^y": equal = Math.Pow(equal, Convert.ToDouble(screen.Text)); break;
                         }
                         check2 = false;
-                        if (task != "C" ) { screen.Text = equal.ToString(); }
+                        if (task != "C") { screen.Text = equal.ToString(); }
                     }
                     check = true;
                     switch (data)
-                        {
-                            case "C":
-                                check = false;
-                                check2 = true;
-                                task = "";
-                                equal = .0;
-                                screen.Text = "";
-                                break;
-                            case "<-":
-                                string s = screen.Text;
-                                check = false;
-                                check2 = false;
-                                if (s.Length > 1)
-                                    s = s.Substring(0, s.Length - 1);
-                                else
-                                    s = "";
-                                screen.Text = s;
-                                break;
-                        }
+                    {
+                        case "C":
+                            check = false;
+                            check2 = true;
+                            task = "";
+                            equal = .0;
+                            screen.Text = "";
+                            break;
+                        case "<-":
+                            string s = screen.Text;
+                            check = false;
+                            check2 = false;
+                            if (s.Length > 1)
+                                s = s.Substring(0, s.Length - 1);
+                            else
+                                s = "";
+                            screen.Text = s;
+                            break;
+                    }
                     if (data != "=")
                         task = data;
                     else
@@ -94,20 +136,30 @@ namespace Kalkulator
                 }
                 else
                 {
-                    if(data == "+/-")
+                    if (data == "+/-")
                     {
                         double d = Convert.ToDouble(screen.Text);
                         d *= -1;
                         screen.Text = d.ToString();
                     }
-                    else 
+                    else
                     {
                         screen.Text += data;
                         check2 = true;
                     }
                 }
             }
-            catch(Exception exc) { MessageBox.Show("U are too dumb to even use a calculator and this is why" + exc.Message, "You dumb", MessageBoxButton.OK, MessageBoxImage.Warning); }
+            catch (Exception exc) { MessageBox.Show("U are too dumb to even use a calculator and this is why" + exc.Message, "You dumb", MessageBoxButton.OK, MessageBoxImage.Warning); }
+        }
+
+        private void Function_Button(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void OneParametr_function_button(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Kalkulator/obj/Debug/net6.0-windows/App.g.cs
+++ Kalkulator/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Kalkulator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Kalkulator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             Kalkulator.App app = new Kalkulator.App();
             app.InitializeComponent();
